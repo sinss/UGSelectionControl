f63306955c27287f108a75129b74ac6ec1b6dad1
leo.chang <leo.chang@unigreen.io>
2016-08-19 10:09:47 +0800

    Initial Commit

---
+++ UniSelectionField/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UniSelectionField
+//
+//  Created by Leo Chang on 8/19/16.
+//  Copyright © 2016 Unigreen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
